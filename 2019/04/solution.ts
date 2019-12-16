0e8d17c01a74c5b59ded3bc2c0c6b181348b018f
Finn Poppinga <finno.ger@gmail.com>
2019-12-16 22:40:03 +0100

    Day 4 brute force

---
+++ 2019/04/solution.ts
@@ -0,0 +1,27 @@
+
+const from = 356_261;
+const to = 846_303;
+
+let count = 0;
+for (let candidate = from; candidate <= to; candidate++) {
+    const digits = (candidate.toString()).split("").map(c => parseInt(c));
+
+    let doubleDigit = false;
+    let increasing = true;
+    for (let i = 0; i < 6; i++) {
+        // increasing
+        if (digits[i + 1] < digits[i]) {
+            increasing = false;
+            break;
+        }
+
+        // contains double digit:
+        doubleDigit = doubleDigit || digits[i - 1] === digits[i];
+    }
+
+    if (increasing && doubleDigit) {
+        count++;
+    }
+}
+
+console.info("Count:", count);
